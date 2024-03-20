8f4a30a13ebb2c82153bac7316e7609f62bddec7
muhammadrazaq33 <muhammadrazaq5913483@gmail.com>
2024-03-21 03:14:51 +0500

    Google Calender Page Completed

--- src/GoogleCalender/GoogleCalender.jsx
+++ src/GoogleCalender/GoogleCalender.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import GoogleCalenderHero from "./GoogleCalenderHero";
 import GoogleCalenderSection from "./GoogleCalenderSection";
+import GoogleCalenderFooter from "./GoogleCalenderFooter";
+import WorkSpaceFooter from "../Components/WorkSpaceFooter";
 
 const GoogleCalender = () => {
   return (
     <div>
       <GoogleCalenderHero />
       <GoogleCalenderSection />
+      <GoogleCalenderFooter />
+      <WorkSpaceFooter />
     </div>
   );
 };
