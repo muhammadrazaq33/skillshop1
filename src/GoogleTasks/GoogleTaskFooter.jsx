7db03618fae98786ab10fb06035dfdfc2707a62c
muhammadrazaq33 <muhammadrazaq5913483@gmail.com>
2024-03-21 22:19:30 +0500

    task page completed

---
+++ src/GoogleTasks/GoogleTaskFooter.jsx
@@ -0,0 +1,86 @@
+import React from "react";
+
+const GoogleTaskFooter = () => {
+  return (
+    <div>
+      {/* seCond LAST FOoTeR */}
+      <div className="mt-[2rem]">
+        <img src="./assets/clip.png" alt="" />
+      </div>
+      {/* footer section =======> */}
+      <div className="bg-[#F9F9FA] pt-[2.2rem] pb-[4rem]">
+        <div className="max-w-[960px] w-[92vw] m-auto flex flex-col gap-4">
+          <h1 className="text-[30px] text-[#ea8600]">Knowledge check</h1>
+
+          {/* TABLE  ========> */}
+          <div className="table-container px-6 pt-10 pb-6 flex flex-col gap-5">
+            <h1 className="text-[20px]">
+              Which of the following does <strong>NOT</strong> describe a
+              feature of Google Keep?
+            </h1>
+
+            {/* CHECKS  ========>*/}
+            <div className="grid sm:grid-cols-2 grid-cols-1 gap-1">
+              <article className="flex items-center gap-4 border-[#e37400] border-[2px] py-4 px-5 rounded-md hover:bg-[#e37400] hover:text-white">
+                <input
+                  type="checkbox"
+                  className="form-checkbox border-2 flex-shrink-0   h-5 w-5"
+                  name=""
+                  id=""
+                />
+                <p className="text-[16px]">
+                  You can record multiple notes to brainstorm ideas
+                </p>
+              </article>
+              <article className="flex items-center gap-4 border-[#e37400] border-[2px] py-4 px-5  rounded-md hover:bg-[#e37400] hover:text-white">
+                <input
+                  type="checkbox"
+                  name=""
+                  className="flex-shrink-0 h-5 w-5"
+                  id=""
+                />
+                <p className="text-[16px]">Keep is a note-taking app</p>
+              </article>
+              <article className="flex items-center gap-4 border-[#e37400] border-[2px] py-4 px-5  rounded-md hover:bg-[#e37400] hover:text-white">
+                <input
+                  type="checkbox"
+                  name=""
+                  className="flex-shrink-0 h-5 w-5"
+                  id=""
+                />
+                <p className="text-[16px]">
+                  You can share and collaborate on notes
+                </p>
+              </article>
+              <article className="flex items-center gap-4 border-[#e37400] border-[2px] py-4 px-5  rounded-md hover:bg-[#e37400] hover:text-white">
+                <input
+                  type="checkbox"
+                  name=""
+                  className="flex-shrink-0 h-5 w-5"
+                  id=""
+                />
+                <p className="text-[16px]">
+                  Keep uses smart chips to add calendar events and other Google
+                  documents to a note
+                </p>
+              </article>
+            </div>
+
+            {/* bUTtONS */}
+            <div className="grid grid-cols-2 gap-2">
+              <button className="bg-white py-2 table-container1 border border-black">
+                Submit
+              </button>
+              <button className="bg-white py-2 table-container1 border border-black">
+                Show Feedback
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* END OF SECOND LasT fOoter */}
+    </div>
+  );
+};
+
+export default GoogleTaskFooter;
--- src/GoogleTasks/GoogleTasks.jsx
+++ src/GoogleTasks/GoogleTasks.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import GoogleTaskHero from "./GoogleTaskHero";
 import GoogleTaskSection from "./GoogleTaskSection";
+import GoogleTaskFooter from "./GoogleTaskFooter";
+import WorkSpaceFooter from "../Components/WorkSpaceFooter";
 
 const GoogleTasks = () => {
   return (
     <div>
       <GoogleTaskHero />
       <GoogleTaskSection />
+      <GoogleTaskFooter />
+      <WorkSpaceFooter />
     </div>
   );
 };
