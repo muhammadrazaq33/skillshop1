81a2c8eae56f373d41510f1369684035ce387ddc
muhammadrazaq33 <muhammadrazaq5913483@gmail.com>
2024-03-22 20:45:27 +0500

    Completed

--- src/GoogleClassroom2/GoogleClassroom2.jsx
+++ src/GoogleClassroom2/GoogleClassroom2.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import GoogleClassHero from "./GoogleClassHero";
+import ClassSection from "./ClassSection";
 
 const GoogleClassroom2 = () => {
   return (
     <div>
       <GoogleClassHero />
+      <ClassSection />
     </div>
   );
 };
