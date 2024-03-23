74cc1a70e4e88bda9accf2a7c41ab6b7c01a2d76
muhammadrazaq33 <muhammadrazaq5913483@gmail.com>
2024-03-23 06:53:54 +0500

    DigItal feedback page completed

--- src/DigitalFeedback/DigitalFeedback.jsx
+++ src/DigitalFeedback/DigitalFeedback.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import DigitalFeedbackHero from "./DigitalFeedbackHero";
 import DigitalFeedbackSection from "./DigitalFeedbackSection";
+import DigitalFeedbackFooter from "./DigitalFeedbackFooter";
+import WorkSpaceFooter from "../Components/WorkSpaceFooter";
 
 const DigitalFeedback = () => {
   return (
     <div>
       <DigitalFeedbackHero />
       <DigitalFeedbackSection />
+      <DigitalFeedbackFooter />
+      <WorkSpaceFooter />
     </div>
   );
 };
